d881c85532bdb8319f52301f252489d37f33a52f
wyz44 <80086632+wyz44@users.noreply.github.com>
2021-07-31 18:34:51 +0800

    新增ListView長按移除功能 項目,金額成功放上 ListView

--- app/src/main/java/com/example/keep/MainActivity.java
+++ app/src/main/java/com/example/keep/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.keep;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listview;
-    private String[] from = {"title", "num"};
-    private int[] to = {R.id.item_title, R.id.num};
+    private String[] from = {"num", "item", "cost"};
+    private int[] to = {R.id.num, R.id.item_title, R.id.cost_title};
     private LinkedList<HashMap<String,String>> data = new LinkedList<>();
     private SimpleAdapter adapter;
-    private EditText scan;
+    private EditText cost, item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         listview = findViewById(R.id.list);
         initListView();
+
+        //長按出現刪除對話框
+        setListItemLongClick();
+
     }
 
     private void initListView(){
@@ -47,21 +55,58 @@ public class MainActivity extends AppCompatActivity {
         listview.setAdapter(adapter);
     }
 
-    public void add(View view) {
-        HashMap<String,String> d0 = new HashMap<>();
-        scan = findViewById(R.id.scan);
-        d0.put("title", scan.getText().toString());
-        d0.put("num", "" + (data.size()+1));
-        data.add(d0);
-        adapter.notifyDataSetChanged();
-        scan.setText("");
-        listview.smoothScrollToPosition(data.size()-1);
+
+    private void setListItemLongClick() {
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                           final int position, long id) {
+                //定義AlertDialog.Builder物件，當長按列表項的時候彈出確認刪除對話方塊
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("確定刪除?");
+                builder.setTitle("提示");
+
+                //新增AlertDialog.Builder物件的setPositiveButton()方法
+                builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(data.remove(position)!=null){
+                            System.out.println("success");
+                        }else {
+                            System.out.println("failed");
+                        }
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(getBaseContext(), "刪除列表項", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                //新增AlertDialog.Builder物件的setNegativeButton()方法
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.create().show();
+                return true;
+            }
+        });
     }
 
-    public void del(View view) {
-        if(data.size()>0){
-            data.remove(0);
-        }
+
+    public void add(View view) {
+        HashMap<String,String> d0 = new HashMap<>();
+        item = findViewById(R.id.item);
+        cost = findViewById(R.id.cost);
+        d0.put("num", "" + (data.size()+1));
+        d0.put("item", item.getText().toString());
+        d0.put("cost", cost.getText().toString());
+        data.add(d0);
         adapter.notifyDataSetChanged();
+        item.setText("");
+        cost.setText("");
+        listview.smoothScrollToPosition(data.size()-1);
     }
 }
